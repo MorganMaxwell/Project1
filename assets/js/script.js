88d16814a5d0626706ba7c4cdd7353116cd37a73
fkakatie <sextonskatie@gmail.com>
2018-12-15 10:11:26 -0700

    added masonry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 console.log('The JavaScript file is linked.');
+
+// setup masonry grid
+$('.masonry').masonry({
+    itemSelector: '.mason-item',
+    columnWidth: 300,
+    gutter: 15
+});
--- logic.js
+++ logic.js
@@ -1,1 +1,3 @@
 console.log("Goodbye, Cruel World");
+
+
