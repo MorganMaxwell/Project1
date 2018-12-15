32af2f21ebd37357991e0d9aa26ef9a219966748
Andrew Waterhouse <gudolph@gmail.com>
2018-12-14 23:05:18 -0700

    I have set up the NYT API, but since it's a free server they have a limit on the number of requests you can ask for at a time.  We need to figure out a way to cull down/ choose more important events so that the server doesn't cut off anymore.

--- assets/Logic.js
+++ assets/Logic.js
@@ -4,8 +4,13 @@
 //easy to read JSON.
 
 var queryUrl = "https://history.muffinlabs.com/date/";
-var date = "2/14";
+
+//the date variable will be acquired from the moment.js, here I just filled it in with an example.  The API is set to recieve "mm/dd" format.
+//the nyt requires a date variable in "mmdd" without the "/"
+var date = "02/14";
+var nytDate = "0214";
 var keyArray = [];
+var dateArray = [];
 $.ajax({
     url: queryUrl + date,
     method: "GET",
@@ -16,21 +21,35 @@ $.ajax({
     for (var i = 0; i < response.data.Events.length; i++) {
         var y = parseInt(response.data.Events[i].year);
         if (y >= 1851 && y <= 2018) {
-            keyArray.push(response.data.Events[i].links[0].title)
+            keyArray.push(response.data.Events[i].links[0].title);
+            dateArray.push(response.data.Events[i].year);
         }
     }
     console.log(keyArray);
+    console.log(dateArray);
+    //Create the components for the API query to the NYT
+    var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+    for (var n = 0; n < keyArray.length; n++) {
+        var begin_date = dateArray[n] + nytDate;
+        var q = keyArray[n];
+
+
+        nytUrl += '?' + $.param({
+            'api-key': "23b3fa378bcf46cb8aa4f6c7d3aa7070",
+            'q': q,
+            'begin_date': begin_date,
+            'end_date': begin_date,
+            'fl': 'headline'
+        });
+        var api = "api-key=23b3fa378bcf46cb8aa4f6c7d3aa7070";
+
+        $.ajax({
+            url: nytUrl + api,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+        });
+    }
 });
 
 
-
-//Create the components for the query URL
-
-/*var url = "http://api.nytimes.com/svc/search/v1/article?query=organicpublication_year:[2004]publication_month:[01]&"
-
-var api = "api-key=23b3fa378bcf46cb8aa4f6c7d3aa7070";
-
-var month = "03";
-var day = "";
-var year = "";
-*/
