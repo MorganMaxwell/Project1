8d8e8440cea47efdc1e4200e96ac2f5afe815142
Morgan M <morgan.maxwell20@gmail.com>
2018-12-17 19:54:49 -0700

    API info pushed to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,87 @@
-console.log('The JavaScript file is linked.');
+$(document).ready(function () {
+    // setup masonry grid
+    $('.masonry').masonry({
+        columnWidth: '.mason-sizer',
+        itemSelector: '.mason-item',
+        gutter: 15,
+        percentPosition: true
+    });
 
-// setup masonry grid
-$('.masonry').masonry({
-    columnWidth: '.mason-sizer',
-    itemSelector: '.mason-item',
-    gutter: 15,
-    percentPosition: true
-});
+    // setup materialize date picker
+    $(document).ready(function () {
+        $('.datepicker').datepicker();
+    });
 
-// setup materialize date picker
-$(document).ready(function(){
-    $('.datepicker').datepicker();
-});
+    // this will be the variable we use for the queries
+    var searchDate = moment();
+    var NYTsearch = moment(searchDate).format("MD");
+    var wikiSearch = moment(searchDate).format("M/D");
 
-// this will be the variable we use for the queries
-var searchDate = moment();
-var NYTsearch = moment(searchDate).format("MD");
-var wikiSearch = moment(searchDate).format("M/D");
-
-// grab userInput from searchbar
-function userInput() {
-    if (event.keyCode === 13) {
-        searchDate = $('#date-search').val().trim();
-        var searchKey = "yellow+flowers"
-        var queryUrl = "https://pixabay.com/api/?key=10991575-42d8db2ac1f8661dc432f18af&image_type=photo&q=";
+    // grab userInput from searchbar
+    function userInput() {
+        if (event.keyCode === 13) {
+            searchDate = $('#date-search').val().trim();
+            var searchKey = "yellow+flowers"
+            var queryUrl = "https://pixabay.com/api/?key=10991575-42d8db2ac1f8661dc432f18af&image_type=photo&q=";
 
 
-        $.ajax({
-            url: queryUrl + searchKey,
-            crossDomain: true,
-            method: "GET"
-        }).then(function (response) {
+            $.ajax({
+                url: queryUrl + searchKey,
+                crossDomain: true,
+                method: "GET"
+            }).then(function (response) {
             })
+        };
     };
-};
-// change searchDate to 1 day backwards
-function backButton() {
-    searchDate = moment(searchDate).add(1, 'd');
-};
-// change searchdate to 1 day forwards... it could be fun
-// to just let them change it to future dates because the 
-// code should still run, but it might also look weird
-function nextButton() {
-    searchDate = moment(searchDate).subtract(1, 'd');
-};
+    // change searchDate to 1 day backwards
+    function backButton() {
+        searchDate = moment(searchDate).add(1, 'd');
+    };
+    // change searchdate to 1 day forwards... it could be fun
+    // to just let them change it to future dates because the 
+    // code should still run, but it might also look weird
+    function nextButton() {
+        searchDate = moment(searchDate).subtract(1, 'd');
+    };
+    //First use wikipedia api to get events on this day in history
+    //Wikipedia uses Wikimedia for content and their API doesn't allow for simple accessing the particular content of a particular page.
+    //"muffinlabs" has created an api that fetches this data from the "Today in History" page of wikipedia and parses it into an 
+    //easy to read JSON.
 
-//First use wikipedia api to get events on this day in history
-//Wikipedia uses Wikimedia for content and their API doesn't allow for simple accessing the particular content of a particular page.
-//"muffinlabs" has created an api that fetches this data from the "Today in History" page of wikipedia and parses it into an 
-//easy to read JSON.
-
-var queryUrl = "https://history.muffinlabs.com/date/";
-
-//the date variable will be acquired from the moment.js, here I just filled it in with an example.  The API is set to recieve "mm/dd" format.
-//the nyt requires a date variable in "mmdd" without the "/"
-var date = wikiSearch;
-var keyArray = [];
-var dateArray = [];
-$.ajax({
-    url: queryUrl + date,
-    method: "GET",
-    dataType: "jsonp"
-}).then(function (response) {
-    for (var i = 0; i < response.data.Events.length; i++) {
-        var y = parseInt(response.data.Events[i].year);
-        if (y >= 1851 && y <= 2018) {
-            keyArray.push(response.data.Events[i].text);
-            dateArray.push(response.data.Events[i].year);
-        }
-    } console.log(keyArray, dateArray);
+    var queryUrl = "https://history.muffinlabs.com/date/";
+    
+    //the date variable will be acquired from the moment.js, here I just filled it in with an example.  The API is set to recieve "mm/dd" format.
+    //the nyt requires a date variable in "mmdd" without the "/"
+    var date = wikiSearch;
+    var keyArray = [];
+    var dateArray = [];
+    $.ajax({
+        url: queryUrl + date,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function (response) {
+        for (var i = 0; i < response.data.Events.length; i++) {
+            var y = parseInt(response.data.Events[i].year);
+            if (y >= 1851 && y <= 2018) {
+                keyArray.push(response.data.Events[i].text);
+                dateArray.push(response.data.Events[i].year);
+            }
+        } console.log(keyArray, dateArray);
+        dataPush();
+    });
+    function dataPush() {
+        for (var i = 0; i < keyArray.length; i++) {
+            var div = $('<div>');
+            div.attr('class', 'mason-item');
+            div.html(
+                "<h2>" + dateArray[i] + "</h2>" +
+                "<p>" + keyArray[i] + "</p>"
+            );
+            $('.masonry').append(div).masonry('appended', div);
+        };
+    };
+    // grab data from user, searchbar and buttons
+    $('#date-search').on("keyup", userInput);
+    $('#back-button').on('click', backButton);
+    $('#next-button').on('click', nextButton);
 });
-
-
-// grab data from user, searchbar and buttons
-$('#date-search').on("keyup", userInput);
-$('#back-button').on('click', backButton);
-$('#next-button').on('click', nextButton);
