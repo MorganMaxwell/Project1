3bf206ad870bf7a56ca188d1e34280f31d50e46c
fkakatie <sextonskatie@gmail.com>
2018-12-26 21:22:09 -0700

    a bunch of changes, i'll leave comments

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Project1
-Project 1 using APIS
-PROJECT FOLDER --> Index.html, assets --> JS --> Logic.JS
+# Today in History # 
+What happened today in history? Or yesterday? Or on your birthday? Enter any date in [Today in History](https://morganmaxwell.github.io/Project1/) and find out! 
+
+## What this project does ## 
+This project's `HTML` and `CSS` framework is setup using [`Materialize`](https://materializecss.com/). Dates are captured and parsed using [`Moment.js`](https://momentjs.com/). Historical events are loaded using the [`MuffinLabs API`](https://history.muffinlabs.com/) and images are loaded using [`MediaWiki's API`](https://www.mediawiki.org/wiki/API:Main_page). These events are loaded to the page using `JQuery` and sorted using [`Masonry`](https://masonry.desandro.com/). 
+
+## How users can get started with this project ## 
+1. **Open [Today in History](https://morganmaxwell.github.io/Project1/)**. 
+    - This project will load with historical events from today's date in history. 
+
+2. **Enter a new date**. 
+    - Navigate to the calendar icon at the top of the page, select the date, and choose any day you would like. 
+    - After selecing "OK", new historical events will load at the top of the page. 
+
+3. **Navigate forward or backwards**. 
+    - Users can also change the date by selecting the forward arrow to navigate one day in the future or the back arrow to move one day into the past. 
+    - New historical events will load at the top of the page on each click. 
+
+## Where users can get help with this project ## 
+If you have any questions about this project, initiate a new pull request and leave a comment. 
+
+## Who maintains this project ## 
+This project is maintained by @[gudolph](https://github.com/gudolph), @[MorganMaxwell](https://github.com/MorganMaxwell), @[cforward5674](https://github.com/cforward5674), and @[fkakatie](https://github.com/fkakatie).
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,22 @@
 $(document).ready(function () {
+
     // setup masonry grid
-    $('.masonry').masonry({
+    var msnry = $('.masonry').masonry({
         columnWidth: '.mason-sizer',
         itemSelector: '.mason-item',
         gutter: 15,
         percentPosition: true
     });
 
-    $('.masonry').imagesLoaded( function() {
-        dataPush();
+    msnry.imagesLoaded().progress( function() {
+        msnry.masonry();
     });
 
     // setup materialize date picker
-
-    $('.masonry').masonry({
-        columnWidth: '.mason-sizer',
-        itemSelector: '.mason-item',
-        gutter: 15,
-        percentPosition: true
-    });
     $('.datepicker').datepicker({
         format: 'mmmm d',
     });
+
     // this will be the variable we use for the queries
     var searchDate = moment();
     var NYTsearch = moment(searchDate).format("MD");
@@ -30,28 +25,45 @@ $(document).ready(function () {
 
     $('#date-search').val(searchDisplay);
 
+    // clear ajax arrays
+    function clearArrays() {
+        keyArray = [];
+        dateArray = [];
+        picArray = [];
+        titleArray = [];
+        urlArray = [];
+    }
+
     // grab userInput from searchbar, and grab data
     function userInput() {
         searchDisplay = $('#date-search').val().trim();
         wikiSearch = moment(searchDisplay).format('M/D');
+        clearArrays();
         wikipedia();
     };
+
     // change date 1 day backward
     function backButton() {
         searchDate = moment(searchDate).subtract(1, 'd');
         wikiSearch = moment(searchDate).format("M/D");
         searchDisplay = moment(searchDate).format("MMMM D");
         $('#date-search').val(searchDisplay);
+        console.log(wikiSearch);
+        clearArrays();
         wikipedia();
     };
+
     // change date 1 day forward
     function nextButton() {
         searchDate = moment(searchDate).add(1, 'd');
         wikiSearch = moment(searchDate).format("M/D");
         searchDisplay = moment(searchDate).format("MMMM D");
         $('#date-search').val(searchDisplay);
+        console.log(wikiSearch);
+        clearArrays();
         wikipedia();
     };
+
     //First use wikipedia api to get events on this day in history
     //Wikipedia uses Wikimedia for content and their API doesn't allow for simple accessing the particular content of a particular page.
     //"muffinlabs" has created an api that fetches this data from the "Today in History" page of wikipedia and parses it into an 
@@ -67,6 +79,8 @@ $(document).ready(function () {
     var dateArray = [];
     var picArray = [];
     var titleArray = [];
+    var urlArray = [];
+
     function wikipedia() {
         $.ajax({
             url: queryUrl + wikiSearch,
@@ -78,10 +92,11 @@ $(document).ready(function () {
                 if (y >= 1851 && y <= 2018) {
                     keyArray.push(response.data.Events[i].text);
                     dateArray.push(response.data.Events[i].year);
-                    titleArray.push(response.data.Events[i].links[0].title)
+                    titleArray.push(response.data.Events[i].links[0].title);
+                    urlArray.push(response.data.Events[i].links[0].link);
                 }
             }
-            for (var j = 0; j < 10; j++) {
+            for (var j = 0; j < titleArray.length; j++) {
                 var searchKey = titleArray[j].split(" ").join("_").substring(0, 49);
                 var picUrl = "https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=";
                 var counter = 0;
@@ -92,44 +107,52 @@ $(document).ready(function () {
                     method: "GET"
                 }).then(function (response) {
                     counter++;
-                    console.log(counter);
+                    // console.log(counter);
                     var keys = Object.keys(response.query.pages);
-                    console.log(keys);
+                    // console.log(keys);
                     picArray.push(response.query.pages[keys].original ? response.query.pages[keys].original.source : "#");
-                    console.log(response.query.pages[keys].original.source);
-                    // dataPush();
-                })
+                    // console.log(response.query.pages[keys].original.source);
+                    // console.log('pic array: ' + picArray);
 
+                    dataPush();
+                })
             };
-           
+
+            console.log(dateArray);
+            console.log(keyArray);
+            console.log(urlArray);
+            console.log(picArray);
+            
         });
+
     };
+
     function dataPush() {
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 5; i++) {
             var div = $('<div>');
             var head = $('<h2>');
+            var dateSpan = $('<span class="dateSpan">');
             var desc = $('<p>');
             var img = '<img src="' + picArray[i] + '"/>';
             var btn = $('<a class="waves-effect waves-light btn" target="_blank">')
-            var picIterator = picArray[i];
-
-            console.log(picArray[i]);
 
             div.attr('class', 'mason-item');
-            head.text(dateArray[i]);
+            dateSpan.text(wikiSearch);
+            head.text(dateArray[i]).append(dateSpan);
             desc.text(keyArray[i]);
-            btn.attr('href', 'https://google.com').text('Learn More');
-
-            console.log(picArray[i]);
+            btn.attr('href', urlArray[i]).text('Learn More');
 
             div.append(img, head, desc, btn);
 
             $('.masonry').prepend(div).masonry('prepended', div);
         };
     };
+
     wikipedia();
+
     // grab data from user, searchbar and buttons
     $('.datepicker-done').on("click", userInput);
     $('#backButton').on('click', backButton);
     $('#nextButton').on('click', nextButton);
+
 });
